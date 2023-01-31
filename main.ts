5a8a110f7efb6281ff770cca07873b36924045f4
aiden351 <122555616+aiden351@users.noreply.github.com>
2023-01-31 12:07:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,53 @@
-function Update_scores () {
+input.onButtonPressed(Button.A, function () {
+    PA += 1
+    Rounds += 1
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        # . . . #
+        # . . . #
+        `)
+    update_score()
+})
+input.onButtonPressed(Button.AB, function () {
+    Rounds += 1
+    Ties += 1
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    update_score()
+})
+input.onButtonPressed(Button.B, function () {
+    PB += 1
+    Rounds += 1
+    basic.showLeds(`
+        # # . . .
+        # . # . .
+        # # # . .
+        # . # . .
+        # # . . .
+        `)
+    update_score()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # . . #
+        `)
+    reset()
+    Rounds = 0
+    Ties = 0
+    update_score()
+})
+function reset () {
     OLED.clear()
     OLED.writeStringNewLine("Player A:" + PA)
     OLED.newLine()
@@ -8,15 +57,18 @@ function Update_scores () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds:" + Rounds)
 }
-let Rounds = 0
-let Ties = 0
+function update_score () {
+    OLED.init(128, 64)
+    PA = 0
+    PB = 0
+    Ties = 0
+    Rounds = 0
+    OLED.writeStringNewLine("\"Let's play a game\"")
+    basic.pause(2000)
+    update_score()
+}
 let PB = 0
+let Ties = 0
+let Rounds = 0
 let PA = 0
-OLED.init(128, 64)
-PA = 0
-PB = 0
-Ties = 0
-Rounds = 0
-OLED.writeStringNewLine("\"Let's play a game\"")
-basic.pause(2000)
-Update_scores()
+reset()
